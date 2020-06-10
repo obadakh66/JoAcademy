e6a4ee186e2131d47756f62518bfa787f766b3a5
obada.alkhdor <obada.alkhdor@technzone.com>
2020-06-11 00:31:16 +0300

    sharing buttons

--- src/app/cumulators/result-dialog/result-dialog.ts
+++ src/app/cumulators/result-dialog/result-dialog.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'modal-page',
@@ -11,7 +13,7 @@ export class ModalPage implements OnInit {
   @Input() avgType: any;
   percentageAvg: any;
   numberAvg: any;
-  constructor(public modalCtrl: ModalController) { }
+  constructor(public modalCtrl: ModalController, private socialSharing: SocialSharing, private langService: LanguageService) { }
   dismiss() {
     this.modalCtrl.dismiss({
       'dismissed': true
@@ -19,36 +21,55 @@ export class ModalPage implements OnInit {
   }
   ngOnInit() {
     console.log(this.result)
-    this.result =this.result.substring(0, this.result.indexOf('.')+3)
+    this.result = this.result.substring(0, this.result.indexOf('.') + 3)
     console.log(this.result)
     if (this.avgType == 1) {
       this.percentageAvg = this.result
-      this.numberAvg=(Number(this.result)/100) *4
+      this.numberAvg = (Number(this.result) / 100) * 4
     }
     if (this.avgType == 2) {
-      this.percentageAvg = (Number(this.result)/4)*100
-      this.numberAvg=this.result
+      this.percentageAvg = (Number(this.result) / 4) * 100
+      this.numberAvg = this.result
     }
   }
-  checkResultString(){
-    let resultNumber=this.percentageAvg;
-      if(resultNumber<=50){
-        return '1'
-      }
-      if(resultNumber>50 && resultNumber<=60){
-        return '2'
-      }
-    if(resultNumber>60 && resultNumber<=70){
-        return '3'
-      }
-    if(resultNumber>70 && resultNumber<=80){
-        return '4'
-      }
-    if(resultNumber>80 && resultNumber<=90){
-        return '5'
-      }
-    if(resultNumber>90 && resultNumber<=100){
-        return '6'
+  shareWhatsapp() {
+    this.socialSharing.shareViaWhatsApp(
+      this.langService.isArabic() ? `معدلي التراكمي الجامعي على تطبيق جو اكاديمي هو :   ${this.percentageAvg} %` : 
+      `My GPA on (JoAcademy) app is :  ${this.percentageAvg} %`,
+      null,
+      "https://play.google.com/store/apps/details?id=io.joacademy.studentapp");
+  }
+  shareFacebook() {
+    this.socialSharing.shareViaFacebook(this.langService.isArabic() ? `معدلي التراكمي الجامعي على تطبيق جو اكاديمي هو :   ${this.percentageAvg} %` : 
+    `My GPA on (JoAcademy) app is :  ${this.percentageAvg} %`,
+    null,
+    "https://play.google.com/store/apps/details?id=io.joacademy.studentapp");
+  } 
+  shareInstagram() {
+    this.socialSharing.shareViaInstagram(this.langService.isArabic() ? `معدلي التراكمي الجامعي على تطبيق جو اكاديمي هو :   ${this.percentageAvg} %` : 
+    `My GPA on (JoAcademy) app is :  ${this.percentageAvg} %`,
+    null
+    );
+  }
+  checkResultString() {
+    let resultNumber = this.percentageAvg;
+    if (resultNumber <= 50) {
+      return '1'
+    }
+    if (resultNumber > 50 && resultNumber <= 60) {
+      return '2'
+    }
+    if (resultNumber > 60 && resultNumber <= 70) {
+      return '3'
+    }
+    if (resultNumber > 70 && resultNumber <= 80) {
+      return '4'
+    }
+    if (resultNumber > 80 && resultNumber <= 90) {
+      return '5'
+    }
+    if (resultNumber > 90 && resultNumber <= 100) {
+      return '6'
     }
   }
 }
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,11 +2,12 @@
 
 import { NgModule } from '@angular/core';
 
-import {CommonModule} from '@angular/common';
-import {IonicModule} from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import { HeaderPage } from './header/header.page';
 import { TranslateModule } from '@ngx-translate/core';
 import { ModalPage } from './cumulators/result-dialog/result-dialog';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @NgModule({
     imports: [
@@ -14,8 +15,9 @@ import { ModalPage } from './cumulators/result-dialog/result-dialog';
         IonicModule,
         TranslateModule.forChild()
     ],
-    declarations: [HeaderPage,ModalPage],
-    entryComponents:[ModalPage],
-    exports: [HeaderPage,ModalPage]
+    declarations: [HeaderPage, ModalPage],
+    entryComponents: [ModalPage],
+    providers: [SocialSharing],
+    exports: [HeaderPage, ModalPage]
 })
-export class SharedModule {}
+export class SharedModule { }
